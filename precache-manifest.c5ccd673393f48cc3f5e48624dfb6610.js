d93e7ddb7ea9317bb75d9f1aae2ee2ae2a206d58
Alireza <alireza.az76@gmail.com>
2020-09-21 11:17:44 +0300

    Updates

--- precache-manifest.c5ccd673393f48cc3f5e48624dfb6610.js
+++ precache-manifest.c5ccd673393f48cc3f5e48624dfb6610.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a82edc8847e46d069b1a9fedeaf85c80",
+    "revision": "bb4495aefb1caf549e356807689970cb",
     "url": "/mindly-crypto/index.html"
   },
   {
-    "revision": "e49ca4fd47c9bcf11fa1",
+    "revision": "48a77fcbe346d35edf07",
     "url": "/mindly-crypto/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "60f02967e2ef05283087",
+    "revision": "b86a599dcb32e8f02c31",
     "url": "/mindly-crypto/static/css/main.821d5153.chunk.css"
   },
   {
-    "revision": "e49ca4fd47c9bcf11fa1",
-    "url": "/mindly-crypto/static/js/2.20d507e2.chunk.js"
+    "revision": "48a77fcbe346d35edf07",
+    "url": "/mindly-crypto/static/js/2.ae8a0802.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/mindly-crypto/static/js/2.20d507e2.chunk.js.LICENSE.txt"
+    "url": "/mindly-crypto/static/js/2.ae8a0802.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "60f02967e2ef05283087",
-    "url": "/mindly-crypto/static/js/main.3858816d.chunk.js"
+    "revision": "b86a599dcb32e8f02c31",
+    "url": "/mindly-crypto/static/js/main.eb0d21b6.chunk.js"
   },
   {
     "revision": "3f6790629903f72e4751",
