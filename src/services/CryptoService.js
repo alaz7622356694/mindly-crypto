42ed6aea68f7c223b3187afc988beb1531167395
Alireza <alireza.az76@gmail.com>
2020-09-21 12:48:09 +0300

    modification

--- src/services/CryptoService.js
+++ src/services/CryptoService.js
@@ -12,8 +12,8 @@ class CryptoService {
     return axios.get(CRYPTO_API_BASE_URL + "/" + cryptoId).then((res) => res);
   }
 
-  addCrypto(cryptoId) {
-    return axios.post(CRYPTO_API_BASE_URL, cryptoId).then((res) => res);
+  addCrypto() {
+    return axios.post(CRYPTO_API_BASE_URL).then((res) => res);
       
   }
 
