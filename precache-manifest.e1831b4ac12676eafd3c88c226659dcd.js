269099c40b4bbe0d670531a52df17443e6fe7daf
Alireza <alireza.az76@gmail.com>
2020-09-21 10:35:16 +0300

    Updates

---
+++ precache-manifest.e1831b4ac12676eafd3c88c226659dcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6f2100c2948471becd27a63d5a51ab",
+    "url": "/mindly-crypto/index.html"
+  },
+  {
+    "revision": "48a77fcbe346d35edf07",
+    "url": "/mindly-crypto/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "88a5e58323ec8dc26135",
+    "url": "/mindly-crypto/static/css/main.821d5153.chunk.css"
+  },
+  {
+    "revision": "48a77fcbe346d35edf07",
+    "url": "/mindly-crypto/static/js/2.ae8a0802.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mindly-crypto/static/js/2.ae8a0802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a5e58323ec8dc26135",
+    "url": "/mindly-crypto/static/js/main.b177e587.chunk.js"
+  },
+  {
+    "revision": "3f6790629903f72e4751",
+    "url": "/mindly-crypto/static/js/runtime-main.67749081.js"
+  }
+]);
